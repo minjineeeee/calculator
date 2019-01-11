36df1a8d9923a5aed5bd5e2b04a0ff3faf69799b
권민진 <alswls101777@gmail.com>
2019-01-11 16:33:05 +0900

    : 를 추가하였습니다.

--- main-1.c
+++ main-1.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("Please enter a expression");
+	printf("Please enter a expression: ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
@@ -57,4 +57,3 @@ int div(int number1, int number2)
 	//TODO implementation
 	return 0;
 }
-#수정
